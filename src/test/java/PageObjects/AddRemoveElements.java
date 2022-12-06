9c927ec132fb77179b7c8152ce0a42b2363ecfa5
Sofya Odegova <sophiaodegova@mail.ru>
2022-12-06 09:41:11 +0400

    fixed all

--- src/test/java/PageObjects/AddRemoveElements.java
+++ src/test/java/PageObjects/AddRemoveElements.java
@@ -1,27 +1,29 @@
 package PageObjects;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class AddRemoveElements {
     public static SelenideElement addRemoveElements = $x("//*[text()=\"Add/Remove Elements\"]");
     public static SelenideElement addElementButton = $x("//*[text()=\"Add Element\"]");
-    public static SelenideElement deleteButton = $x("//*[text()=\"Delete\"]");
+    public static ElementsCollection deleteButton = $$x("//*[text()=\"Delete\"]");
 
     public static void addRemove(Integer count) {
 
         addRemoveElements.click();
 
         System.out.println("Переходим на страницу /add_remove_elements");
-        for (int i = 1; i <= count; i++) {
+        for (int i = 0; i < count; i++) {
             addElementButton.click();
         }
         System.out.println("Добавили кнопку delete " + count + " раз");
 
-        for (int j = 1; j <= count; j++) {
-            deleteButton.shouldBe(Condition.visible);
+        for (int j = 0; j < count; j++) {
+            deleteButton.get(j).shouldBe(Condition.visible);
         }
     }
 }
